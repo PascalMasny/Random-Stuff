bf2f0a2b5a0bf3a643267deeeeab2715ff17233e
Passi <62151638+PascalMasny@users.noreply.github.com>
2021-06-04 00:11:50 +0200

    DEL

--- Mathlab/helix.m
+++
@@ -1,7 +0,0 @@
-t = 0:0.1:10*pi;
-r = linspace (0, 1, numel (t));
-z = linspace (0, 1, numel (t));
-plot3 (r.*sin(t), r.*cos(t), z);
-
-saveas(gcf,'helix.png') #safe plot as a picture
-
--- Mathlab/test_datatypes/main.py
+++
@@ -1,8 +0,0 @@
-#https://blink1073.github.io/oct2py/source/examples.html
-
-from oct2py import octave
-import pprint
-
-out = octave.test_dataypes()
-
-pprint.pprint(out)
