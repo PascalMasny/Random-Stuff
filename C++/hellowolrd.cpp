7bf2675ccd070675f510811d527eac6bce795deb
Pascal Masny <pascal.masny@protonmail.com>
2021-12-30 18:02:48 +0100

    add C++ HelloWorld

---
+++ C++/hellowolrd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  //Hello there!
+  cout << "Hello World! \n";
+  cout << "I am a C++ Program";
+
+  /* 
+  Ahhh General Kenobi
+  */
+  return 0;
+}
